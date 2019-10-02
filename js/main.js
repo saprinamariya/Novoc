f7b32aac6af2a8cc51389c79e68223d579d16f64
maria <saprina.mariya@mail.ru>
2019-10-03 01:00:46 +0300

    Developed form

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
 document.querySelector('.search-panel__btn-search').addEventListener('click', function () {
    document.querySelector('.search-panel').classList.add('search-panel_active-search');
 });
-
-$("#slider").slider({
-   min: 0,
-   max: 69,
-   values: [0,69],
-   range: true
-});
